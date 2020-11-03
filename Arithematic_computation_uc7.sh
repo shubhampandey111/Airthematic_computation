3d0cd56b08a1c6c62e4ec1443f18b0970f670c66
shubhampandey111 <shubhampandey051197@gmail.com>
2020-11-03 23:47:51 +0530

    Arithematic Computation UC7

---
+++ Arithematic_computation_uc7.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Welcome To Airthmatic Computation"
+read -p "Enter first number" a
+read -p "Enter second number" b
+read -p "Enter third number" c
+aa=`expr $a + $b \* $c`
+bb=`expr $a % $b + $c`
+cc=`expr $c + $a / $b`
+dd=`expr $a \* $b + $c`
+
+
+declare -A StoringValues
+
+StoringValues[0]=$aa
+StoringValues[1]=$bb
+StoringValues[2]=$cc
+StoringValues[3]=$dd
+
+echo "All values : ${StoringValues[@]} "
+echo "All keys : ${!StoringValues[@]} "
+echo " Elements : ${#StoringValues[@]}  "
+
+ab()
+{
+
+for i in ${!StoringValues[@]}
+do
+StoringArray[$i]=${StoringValues[$i]}
+done
+echo ${StoringArray[@]}
+
+n=$(("${#StoringArray[@]}"))
+echo $n
+}
+ab
